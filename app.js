4b40486c40aba206c5be2f9ae042ae1bd040d1d1
NoeFabris <noe@elysresidence.com>
2021-05-13 01:37:18 +0200

    Pt.2

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
 new Vue({
     el: '#root',
     data: {
-        albumsUnfiltered: [],
-    },
-    methods: {
+        albums: [],
+        selectedGenre: '',
+        genresFiltered: [],
 
     },
+    computed: {
+
+    },
+    methods: {
+        
+        genresSelect: function () {
+            if (this.selectedGenre === 'all') {
+              this.albums = this.albums
+
+            } else {
+
+                this.genresFiltered.forEach(genre => {
+                    if (this.selectedGenre == genre) {
+                        
+                    }
+                });
+
+            }
+        }
+    },
     mounted() {
+        
         axios.get('https://flynn.boolean.careers/exercises/api/array/music')
             .then((resp) => {
-            this.albumsUnfiltered = resp.data.response;
-        })
+                this.albums = resp.data.response;
+                
+                this.albums.forEach(album => {
+
+                    if (!this.genresFiltered.includes(album.genre)){
+                        this.genresFiltered.push(album.genre)
+                    }
+                });
+                
+                // this.genresFiltered = this.genresUnfiltered
+                // this.genresFiltered.filter((genre, index) => this.genresFiltered.indexOf(genre) !== index)
+            })
+        
     }
 })
